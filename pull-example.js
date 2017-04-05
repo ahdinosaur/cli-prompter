d9c6b58ea2831c3c4f1bd49b8543826ff0a632e6
Jamen Marzonie <jamenmarz@gmail.com>
2017-04-04 20:17:47 -0500

    Test in #1 with pull-stream-model

--- pull-example.js
+++ pull-example.js
@@ -1,30 +1,58 @@
 const pull = require('pull-stream')
-const pullNotify = require('pull-notify')
+const notify = require('pull-notify')
+const component = require('pull-stream-model')
 const prompter = require('./')
 
-var questions = pullNotify()
-
-var currentLocation = {
-  x: 0,
-  y: 1
+function navigator () {
+  return { x: 0, y: 0 }
 }
+
+navigator.update = {
+  move: ({x, y}, direction) => {
+    console.log(x, y, direction)
+    switch (direction) {
+      case 'north': return {x, y: y + 1}
+      case 'south': return {x, y: y - 1}
+      case 'east': return {x: x + 1, y}
+      case 'west': return {x: x - 1, y}
+      default: return {x, y}
+    }
+  }
+}
+
+var questions = notify()
+var answers = notify()
+var model = component(navigator)
+
+function ask () {
+  questions({
+    type: 'text',
+    message: 'Which direction do you want to go?',
+    default: 'north'
+  })
+}
+
+pull(
+  answers.listen(),
+  model.store,
+  // log updates
+  pull.through(console.log),
+  pull.collect(function (err, state) {
+    // handle if application errored:
+    if (err) throw err
+    // serialize/reuse end state:
+    console.log(state)
+  })
+)
 
 pull(
   questions.listen(),
   prompter(),
-  pull.drain(values => {
-    console.log('value', values)
-    questions(nextQuestion(values))
+  pull.drain(answer => {
+    answers(model.msg.move(answer.undefined))
+    ask()
   })
 )
 
-questions(nextQuestion())
-
-function nextQuestion (values) {
-  return {
-    type: 'text',
-    name: 'move',
-    message: 'What direction do you want to move in?',
-    default: 'south'
-  }
-}
+// Do the first ask
+ask()
